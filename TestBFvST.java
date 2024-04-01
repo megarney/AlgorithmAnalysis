aaa3476dd63afc651145db6f3384c83581811a54
megarney <megarney0@gmail.com>
2024-04-01 16:53:29 -0500

    Exploration2

--- TestBFvST.java
+++ TestBFvST.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
-import java.time.Instant;
-import java.time.Duration;
+import java.util.HashMap;
 
-public class TestBF {
+public class TestBFvST {
 
     public static int iterationCount = 0;
     public static long start;
@@ -43,7 +41,7 @@ public class TestBF {
         int ninhund[] = new int[900];
         int nisione[] = new int[961];
         int max[] = new int[1024];
-
+         
         experiment(one);
         experiment(four);
         experiment(nine);
@@ -88,17 +86,20 @@ public class TestBF {
         }
 
         iterationCount = 0;
-        System.out.println(test.length + ": \t| Output: " + nearlyIdentical(test) + " \t| Count: " + iterationCount + " \t| Time: " + time);
+        System.out.print("Input Size: " + test.length + " \t| Output: " + nearlyIdenticalBF(test) + "\t| BF Count: " + iterationCount + "\t| BF Time: " + time);
+        iterationCount = 0;
+        nearlyIdenticalH(test);
+        System.out.print("\t| H Count: " + iterationCount + "\t| H Time: " + time + "\n");
     }
 
-    public static int nearlyIdentical(int[] a){
+    public static int nearlyIdenticalBF(int[] a){
         start = System.nanoTime();
         int longest = 0; // current longest nearly identical subsequence
         int n = a.length;
-        for(int i = 0; i < n - 1; i++){
+        for(int i = 0; i < n; i++){
             int count = 0; // Number of values = 1 more than current value
             boolean nextPresent = false; // T/F: a[j] + 1 is present
-            for(int j = 0; j < n - 1; j++){
+            for(int j = 0; j < n; j++){
                 iterationCount++;
                 if(a[j] == a[i]){
                     count = count + 1;
@@ -117,4 +118,31 @@ public class TestBF {
         time = end-start;
         return longest;
     }
+
+    private static int nearlyIdenticalH(int[] inputArray) {
+        start = System.nanoTime();
+        HashMap<Integer, Integer> H = new HashMap<>();
+
+		for (int i = 0; i < inputArray.length; i++) {
+			H.put(inputArray[i], 0);
+            iterationCount++;
+		}
+
+		for (int i = 0; i < inputArray.length; i++) {
+			H.put(inputArray[i], H.get(inputArray[i]) + 1);
+            iterationCount++;
+		}
+
+		int longest = 0;
+
+		for (int key : H.keySet()) {
+            iterationCount++;
+			if (H.containsKey(key + 1)) {
+				longest = Math.max(longest, H.get(key) + H.get(key + 1));
+			}
+		}
+        end = System.nanoTime();
+        time = end-start;
+		return longest;
+	}
 }
