6c45afecd2dff67ba531444fa7f2e209bd3deca4
megarney <megarney0@gmail.com>
2024-04-02 11:58:57 -0500

    changed data input

--- TestBFvST.java
+++ TestBFvST.java
@@ -9,38 +9,21 @@ public class TestBFvST {
 
     public static void main(String[] args) {
 
-        int one[] = {1};
-        int four[] = new int[4];
-        int nine[] = new int[9];
-        int sixteen[] = new int[16];
-        int twfive[] = new int[25];
-        int thsix[] = new int[36];
-        int fonine[] = new int[49];
-        int sifour[] = new int[64];
-        int eione[] = new int[81];
-        int onhund[] = new int[100];
-        int ontwone[] = new int[121];
-        int onfofour[] = new int[144];
-        int onsixnine[] = new int[169];
-        int onnisix[] = new int[196];
-        int twtwfi[] = new int[225];
-        int twfisix[] = new int[256];
-        int tweinine[] = new int[289];
-        int thtwfour[] = new int[324];
-        int thsione[] = new int[361];
-        int fourhund[] = new int[400];
-        int fofoone[] = new int[441];
-        int foeifour[] = new int[484];
-        int fitwnine[] = new int[529];
-        int fisesix[] = new int[576];
-        int sitwfive[] = new int[625];
-        int sisesix[] = new int[676];
-        int setwnine[] = new int[729];
-        int seeifour[] = new int[784];
-        int eifoone[] = new int[841];
-        int ninhund[] = new int[900];
-        int nisione[] = new int[961];
-        int max[] = new int[1024];
+        int one[] = new int[1000];
+        int four[] = new int[2000];
+        int nine[] = new int[3000];
+        int sixteen[] = new int[4000];
+        int twfive[] = new int[5000];
+        int thsix[] = new int[6000];
+        int fonine[] = new int[7000];
+        int sifour[] = new int[8000];
+        int eione[] = new int[9000];
+        int onhund[] = new int[10000];
+        int ontwone[] = new int[11000];
+        int onfofour[] = new int[12000];
+        int onsixnine[] = new int[13000];
+        int onnisix[] = new int[14000];
+        int twtwfi[] = new int[15000];
          
         experiment(one);
         experiment(four);
@@ -57,23 +40,6 @@ public class TestBFvST {
         experiment(onsixnine);
         experiment(onnisix);
         experiment(twtwfi);
-        experiment(twfisix);
-        experiment(tweinine);
-        experiment(thtwfour);
-        experiment(thsione);
-        experiment(fourhund);
-        experiment(fofoone);
-        experiment(foeifour);
-        experiment(fitwnine);
-        experiment(fisesix);
-        experiment(sitwfive);
-        experiment(sisesix);
-        experiment(setwnine);
-        experiment(seeifour);
-        experiment(eifoone);
-        experiment(ninhund);
-        experiment(nisione);
-        experiment(max);
     }
 
     public static void experiment(int[] test){
